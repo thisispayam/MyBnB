17fd98735d8ac6f72ef103b8c64a161e491e8149
acv <acv@yull5fjmxn1.vacv.local>
2018-07-20 16:30:55 -0400

    adding jquery,bootstrap,popper,sass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import {Header} from './shared/Header';
 import './App.css';
-import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Header />
         <h1>MyBnB</h1>
       </div>
     );
---
+++ src/shared/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Header() {
+    return(
+        <p>Header Component</p>
+    )
+}
