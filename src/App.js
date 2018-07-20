bd38a5dba2814071a713cd0264190a052103d7d3
acv <acv@yull5fjmxn1.vacv.local>
2018-07-20 11:29:29 -0400

    initial state

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>MyBnB</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
