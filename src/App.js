8f8cd633acf9bbf03695dbe4c0c04ee95bb47041
Payam <thisispayam@gmail.com>
2018-07-22 23:52:00 -0400

    React Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
 import {Header} from './shared/Header';
 import './App.css';
 import { RentalList } from './components/rental/RentalList';
+import { RentalDetail } from './components/rental/RentalDetail';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Header />
-      <div className='container'>
-       <RentalList />
-      </div>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+        <Header />
+        <div className='container'>
+        <Route exact path='/' component = {RentalList} />
+        <Route exact path='/test' component = {RentalDetail} />
+        </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/rental/RentalDetail.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+
+export class RentalDetail extends Component {
+    render(){
+        return(
+            <p>I'm a rental details component</p>
+        )
+    }
+}
