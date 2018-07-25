158be4896c3c3f54a6eb08696365690228b89288
acv <acv@yull5fjmxn1.vacv.local>
2018-07-25 10:40:17 -0400

    adding redux and seding state from App

--- src/components/rental/RentalList.js
+++ src/components/rental/RentalList.js
@@ -1,71 +1,12 @@
 import React,{Component} from 'react';
 import {RentalCard} from './RentalCard';
+import {connect} from 'react-redux';
 
-export class RentalList extends Component {
-
-    constructor(){
-        super();
-        this.state = {
-            
-                rentals: [{
-                    id: 1,
-                    title: "Central Apartment",
-                    city: "New York",
-                    street: "Times Sqaure",
-                    category: "apartment",
-                    image: "http://via.placeholder.com/350x250",
-                    bedrooms: 3,
-                    description: "Very nice apartment",
-                    dailyRate: 34,
-                    shared: false,
-                    createdAt: "24/12/2017"
-                  },
-                  {
-                    id: 2,
-                    title: "Central Apartment 2",
-                    city: "San Francisco",
-                    street: "Main street",
-                    category: "condo",
-                    image: "http://via.placeholder.com/350x250",
-                    bedrooms: 2,
-                    description: "Very nice apartment",
-                    dailyRate: 12,
-                    shared: true,
-                    createdAt: "24/12/2017"
-                  },
-                  {
-                    id: 3,
-                    title: "Central Apartment 3",
-                    city: "Bratislava",
-                    street: "Hlavna",
-                    category: "condo",
-                    image: "http://via.placeholder.com/350x250",
-                    bedrooms: 2,
-                    description: "Very nice apartment",
-                    dailyRate: 334,
-                    shared: true,
-                    createdAt: "24/12/2017"
-                  },
-                  {
-                    id: 4,
-                    title: "Central Apartment 4",
-                    city: "Berlin",
-                    street: "Haupt strasse",
-                    category: "house",
-                    image: "http://via.placeholder.com/350x250",
-                    bedrooms: 9,
-                    description: "Very nice apartment",
-                    dailyRate: 33,
-                    shared: true,
-                    createdAt: "24/12/2017"
-                }]
-            
-        }
-    }
+class RentalList extends Component {
 
 //rendering the Rental Cards dynamically
     renderRentals(){
-        return this.state.rentals.map((rental,index)=>{
+        return this.props.rentals.map((rental,index)=>{
             return(
                 <RentalCard key={index} colNum='col-md-3 col-xs-6' rental={rental}/>
             )
@@ -82,3 +23,11 @@ export class RentalList extends Component {
         )
     }
 }
+function mapStateToProps(state){
+    return{
+        rentals: state.rentals
+    }
+}
+
+//providing state(rentals) for RentalList in form of props
+export default connect(mapStateToProps)(RentalList);
